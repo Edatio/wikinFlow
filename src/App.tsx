8dc2e66fb9c5bb3d35219860cf4999cdbeefa7b1
harsavardhanSDE <harshavardhan.workspace@gmail.com>
2024-09-06 03:06:22 +0530

    added:

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'; 
+
+const App : React.FC = () => {
+    return (
+        <div>
+            something
+        </div>
+    )
+}
+
+export default App; 
--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,45 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import copy from 'rollup-plugin-copy';
+
+// Convert import.meta.url to a file path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   // Define multiple entry points for background, popup, and options
   build: {
     rollupOptions: {
       input: {
-        background: resolve(__dirname, 'src/background/background.ts'),
-        content: resolve(__dirname, 'src/content/content.ts'),
-        popup: resolve(__dirname, 'src/popup/popup.tsx'),
-        options: resolve(__dirname, 'src/options/options.tsx'),
+        // background: resolve(__dirname, 'src/background/background.ts'),
+        // content: resolve(__dirname, 'src/content/content.ts'),
+        // popup: resolve(__dirname, 'src/popup/popup.tsx'),
+        // options: resolve(__dirname, 'src/options/options.tsx'),
       },
       output: {
         entryFileNames: '[name]/[name].js',
         assetFileNames: 'assets/[name].[ext]',
       },
     },
-    outDir: 'dist',  // Output directory for the built files
+    outDir: 'dist',
   },
   plugins: [
-    react(),  // Enable React fast refresh
+    react(),
+    copy({
+        targets: [{ src : './src/manifest.json', dest: './dist'}],
+        hook: 'writeBundle',
+    })
   ],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),  // Set up alias for `src/` directory
+      '@': resolve(__dirname, 'src'),
     },
   },
   server: {
-    port: 3000,  // Vite development server port
-    open: false,  // Don't open the browser automatically
-    hmr: { overlay: true },  // Enable Hot Module Replacement
+    port: 3000,
+    open: false,
+    hmr: { overlay: true },
   },
 });
